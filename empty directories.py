2c0ca51fe5ee8b2a1e895149aa7ca70f99bef0dd
1phillip <50423954+1phillip@users.noreply.github.com>
2019-11-22 09:58:59 -0800

    Update empty directories.py

--- empty directories.py
+++ empty directories.py
@@ -1,11 +1,7 @@
 import os, sys, shutil
 sizeCutoff = None
-while type(sizeCutoff) != int:
-    try:
-        sizeCutoff=int(input("Look for all files equal to or smaller than (bytes): "))
-    except ValueError:
-        print("Type an integer. \n")
-def get_size(start_path = '.'):
+
+def get_size(start_path = '.'): #define function for retrieving the size of a folder
     global total_size
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
@@ -16,15 +12,23 @@ def get_size(start_path = '.'):
                 total_size += os.path.getsize(fp)
     return total_size
 
+while type(sizeCutoff) != int: #prompt user for the maximum folder size they want in the results
+    try:
+        sizeCutoff = int(input("Look for all files equal to or smaller than (bytes): "))
+    except ValueError:
+        print("Type an integer. \n")
+        
+
+
 listOfDirs = []
-for root, dirs, files in os.walk(".", topdown=False):
+for root, dirs, files in os.walk(".", topdown=False): #get list of folders <= the size specified by the user
    for name in dirs:
        if get_size(os.path.join(root, name)) <= sizeCutoff:
            print(os.path.join(root, name), total_size, 'bytes')
            listOfDirs.append(os.path.join(root, name))
 if len(listOfDirs) > 0:
-    delete=input('Delete? (y/n): ').lower()
-    if delete=='y':
+    delete = input('Delete? (y/n): ').lower() #prompt user to confirm deletion
+    if delete == 'y':
         for i in listOfDirs:
             shutil.rmtree(i)
             print('deleted %s' % i)
